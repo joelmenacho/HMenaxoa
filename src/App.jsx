7a5da17bebee947e40e747ce064321de6bca9ef8
unknown <joelmenacho@gmail.com>
2022-10-14 16:28:40 -0500

    update Views

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { BrowserRouter, Routes, Route } from
-"react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  Producto,
+  Perfil,
+  Carrito,
+  Checkout,
+  Login,
+  NotFound,
+} from "./views";
+  function App(){
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="producto/:id" element={<Producto />} />
+        <Route path="perfil" element={<Perfil />} />
+        <Route path="carrito" element={<Carrito />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    );
+  }
 
-function App() {
 
-  return <div className="text-3xl font-bold underline font-dold">
-      Hello world!
-    </div>;
-
-}
 export default App;
